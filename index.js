eec57b85cf4b3acbdc5c79f22e6437b520dadd9e
Rafa <rafael.valdespino@g-global.com.mx>
2017-01-27 17:37:32 -0800

    server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/bower_components'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname +'/index.html');
